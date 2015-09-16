0136d02ff8728d31811c6e5f0c4783ef7667256a
marb <marb61a@gmail.com>
2015-09-16 08:01:26 +0000

    added stafftickets and updated layout js

--- client/templates/layout.js
+++ client/templates/layout.js
@@ -1,3 +1,10 @@
+// Check to see if user is a staff member
+Template.registerHelper('isStaff', function(){
+	if(Meteor.user().profile.usertype == 'staff'){
+		return true;
+	}
+});
+
 // Use moment.js for date formatting
 Template.registerHelper('formatDate', function(date){
     return moment(date).format('MMMM Do YYYY, h:mm:ss a');
